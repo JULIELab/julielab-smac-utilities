9e96d717158afa5d22d3429385fd2490fbc9991b
khituras <chew@gmx.net>
2020-04-14 10:01:10 +0200

    Added methods to get the n best runs and n best configurations evaluated on all instances (the incumbents).

---
+++ src/main/java/de/julielab/smac/FullyEvaluatedConfiguration.java
@@ -0,0 +1,34 @@
+package de.julielab.smac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FullyEvaluatedConfiguration extends ArrayList<SmacLiveRundataEntry> {
+    private int pcid;
+    private double avgQuality = Double.MAX_VALUE;
+
+    public FullyEvaluatedConfiguration(List<SmacLiveRundataEntry> entries) {
+        super(entries);
+        if (!isEmpty()) {
+            pcid = get(0).getRunInfo().getConfiguration().getPcid();
+        }
+    }
+
+    public double getAvgQuality() {
+        if (avgQuality == Double.MAX_VALUE)
+            throw new IllegalStateException("The average quality has not yet been calculated. Call 'calculateAvgQuality' first.");
+        return avgQuality;
+    }
+
+    public void calculateAvgQuality() {
+        stream().mapToDouble(SmacLiveRundataEntry::getrQuality).average().ifPresent(avg -> avgQuality = avg);
+    }
+
+    public int getPcid() {
+        return pcid;
+    }
+
+    public void setPcid(int pcid) {
+        this.pcid = pcid;
+    }
+}
--- src/main/java/de/julielab/smac/SmacLiveRundata.java
+++ src/main/java/de/julielab/smac/SmacLiveRundata.java
@@ -1,6 +1,7 @@
 package de.julielab.smac;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SmacLiveRundata extends ArrayList<SmacLiveRundataEntry> {
     public SmacLiveRundataEntry getEntryWithBestScore() {
@@ -10,5 +11,39 @@ public class SmacLiveRundata extends ArrayList<SmacLiveRundataEntry> {
                 bestEntry = entry;
         }
         return bestEntry;
+    }
+
+    public List<SmacLiveRundataEntry> getEntriesWithBestScore(int n) {
+        List<SmacLiveRundataEntry> entries = new ArrayList<>(this);
+        // We sort reversed because SMAC minimizes the objective, this lower scores are better
+        Collections.sort(entries, Comparator.comparingDouble(SmacLiveRundataEntry::getrQuality).reversed());
+        return entries.subList(Math.max(entries.size() - n, 0), entries.size());
+    }
+
+    /**
+     * <p>Returns those configurations which have been evaluated in the maximum of instances and have the highest average quality score.
+     * Those were most likely the incumbent as some point in the SMAC configuration phase.</p>
+     * <p>The maximum number of instances is estimated by getting the maximum number of runs for a single configuration.</p>
+     * @param n The number of configurations to return.
+     * @return The best configurations.
+     */
+    public List<FullyEvaluatedConfiguration> getBestFullyEvaluatedConfigurations(int n) {
+        Map<Integer, List<SmacLiveRundataEntry>> pcid2runs = stream().collect(Collectors.groupingBy(e -> e.getRunInfo().getConfiguration().getPcid()));
+        OptionalInt maxRunsPerConfigOpt = pcid2runs.keySet().stream().mapToInt(pcid -> pcid2runs.get(pcid).size()).max();
+        List<FullyEvaluatedConfiguration> fullyEvaluatedConfigurations = new ArrayList<>();
+        if (maxRunsPerConfigOpt.isPresent()) {
+            int maxRunsPerConfig = maxRunsPerConfigOpt.getAsInt();
+            for (List<SmacLiveRundataEntry> entries : pcid2runs.values()) {
+                if (entries.size() == maxRunsPerConfig) {
+                    FullyEvaluatedConfiguration evaluatedConfiguration = new FullyEvaluatedConfiguration(entries);
+                    evaluatedConfiguration.calculateAvgQuality();
+                    fullyEvaluatedConfigurations.add(evaluatedConfiguration);
+                }
+            }
+
+        }
+        // We sort reversed because SMAC minimizes the objective, this lower scores are better
+        Collections.sort(fullyEvaluatedConfigurations, Comparator.comparingDouble(FullyEvaluatedConfiguration::getAvgQuality).reversed());
+        return fullyEvaluatedConfigurations.subList(Math.max(fullyEvaluatedConfigurations.size() - n, 0), fullyEvaluatedConfigurations.size());
     }
 }
